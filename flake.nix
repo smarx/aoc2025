458a5041cf130cb48e6214875fd44fa41760c3f8
smarx <smarx@smarx.com>
2025-12-01 09:30:10 -0800

    get gforth

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Advent of Code 2025";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [ pkgs.gforth ];
+      };
+    };
+}
